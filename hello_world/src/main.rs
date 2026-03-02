28a0874e4aa8d5c4e7aa80657938071bdcec5922
RohanHaugen <haugenrohan@gmail.com>
2026-03-02 14:45:06 -0500

    compound data types

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,6 +1,67 @@
 fn main() {
-    let mut x =10;
-    println!("x is {}", x);
-    x = 20;
-    println!("x is {}",x);
+    
+    /*let stuff = (10, 3.14, 'x');
+    println!("stuff: {}",stuff.0);*/
+
+    //let garage: [[[i32; 100]; 20];5];
+    /*let numbers: [i32; 5];
+    numbers = [0;5];
+    let index: usize = numbers.len();
+    println!("last number is {}",numbers[index]);*/
+
+    /*let a =13;
+    let b =2.3;
+    let c :f32 = 120.0;
+    let average = (a as f64+b +c as f64)/3.0;
+    assert_eq!(average, 45.1);
+    println!("Test Passed");*/
+
+    /*let letter = 'a';
+    let number = '1';
+    let finger ='\u{261D}';*/
+    /*let a =true;
+    let b =false;
+    println!("a is {} and b is {}", a, b);
+    println!("a is EQUAL TO b is {}", a==b);
+    println!("a NOT EQUAL TO b is {}", a!=b);
+    println!("a GREATER THAN b is {}", a>b);
+    println!("a GREATER THAN OR EQUAL TO b is {}", a>=b);
+    println!("a LESS THAN OR EQUAL TO b is {}", a<=b);
+    println!("a LESS THAN b is {}", a<b);*/
+
+    /*let a = true;
+    let b = false;
+    println!("a is {} and b is {}", a,b);
+    println!("NOT a is {}",!a);
+    println!("a AND b is {}", a & b);
+    println!("a OR b is {}", a | b);
+    println!("a XOR b is {}", a ^b);
+    let c = (a ^b)&&panic!();
+    println!("c is {}",c);*/
+    /*let mut value =0b1111_0101u8;
+    println!("value is {}", value);
+    println!("value is {:08b}",value);
+    value = !value;
+    println!("value is {:08b}",value);
+    value = value & 0b1111_0111;
+    println!("value is {:08b}",value);
+    println!("bit 6 is {}", value & 0b0100_0000);
+    value = value | 0b0100_0000;
+    println!("value is {:08b}",value);
+    value = value ^ 0b0101_0101;
+    println!("value is {:08b}",value);
+    value = value << 4;
+    println!("value is {:08b}",value);
+    value = value >> 2;
+    println!("value is {:08b}",value);*/
+    //let mut x: u8 =255;
+    //let y:f32=10.123456789;
+    //println!("x is {}", x);
+    //x = x+1;
+    //println!("x is {}",x);
+    //let a =10;
+    //let b=3.0;
+    //let c = a as f64 /(b+1.0);
+    //println!("c is {0:08.3}\na is {1}\nonce again, c is {0}",c,a);
+
 }
