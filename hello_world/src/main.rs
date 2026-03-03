68208995b07ef1a55338c88571d407bd8902fbb8
RohanHaugen <haugenrohan@gmail.com>
2026-03-02 20:36:24 -0500

    program flow control

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,8 +1,34 @@
 fn main() {
-    let celsius_temp=23.0;
+    let numbers=[1,9,-2,0,23,20,-7,13,37,20,56,-18,20,3];
+    let mut max:i32;
+    let mut min:i32;
+    let mut mean:f64;
+    max=numbers[0];
+    min=numbers[0];
+    mean=0.0;
+
+    for &number in numbers.iter(){
+        if number>max{max=number}else if number<min{min=number}
+        mean+=number as f64;
+    }
+    mean=mean/numbers.len() as f64;
+    assert_eq!(max,56);
+    assert_eq!(min,-18);
+    assert_eq!(mean,12.5);
+    println!("Tests passed");
+    /*let message=['h','e','l','l','o'];
+    for(index,&item) in message.iter().enumerate(){
+        println!("item {} is {}",index,item);
+        if item =='e'{
+            break;
+        }
+    }*/
+    //let make_x_odd=true;
+    //let x= if make_x_odd{1}else{2};
+    /*let celsius_temp=23.0;
     let fahrenheit_temp=celsius_to_fahrenheit(celsius_temp);
     assert_eq!(fahrenheit_temp,73.4);
-    println!("Test Passed");
+    println!("Test Passed");*/
     /*say_hello();
     say_hello();
     let x=1;
